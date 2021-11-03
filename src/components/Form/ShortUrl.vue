6f01bae71fd24c0f47f1214affd9c520331a7100
Rubén Conde <dev@rubn.xyz>
2021-11-03 17:14:42 +0100

    refactor: :recycle: Unnecessary code at form
    Signed-off-by: Rubén Conde <dev@rubn.xyz>

--- src/components/Form/ShortUrl.vue
+++ src/components/Form/ShortUrl.vue
@@ -9,9 +9,6 @@
     <button class="relative px-5 py-2 font-medium text-white group flex-shrink">
       <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-english-lavender group-hover:bg-mountbatten-pink group-hover:skew-x-12"></span>
       <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-mountbatten-pink group-hover:bg-english-lavender group-hover:-skew-x-12"></span>
-
-      <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
-      <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-pastel-pink -rotate-12"></span>
       <span class="relative">Short that URL</span>
     </button>
   </form>
