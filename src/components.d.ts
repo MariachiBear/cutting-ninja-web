f7def4de1cee0b6ce8305412fa2a3941c36c1506
Rubén Conde <dev@rubn.xyz>
2021-11-03 17:07:47 +0100

    feat: :sparkles: Base layaout
    Signed-off-by: Rubén Conde <dev@rubn.xyz>

--- src/components.d.ts
+++ src/components.d.ts
@@ -13,7 +13,9 @@ declare module 'vue' {
     CarbonSun: typeof import('~icons/carbon/sun')['default']
     CarbonWarning: typeof import('~icons/carbon/warning')['default']
     Footer: typeof import('./components/Footer.vue')['default']
+    Hero: typeof import('./components/Hero.vue')['default']
     README: typeof import('./components/README.md')['default']
+    ShortUrl: typeof import('./components/Form/ShortUrl.vue')['default']
   }
 }
 
---
+++ src/components/Form/ShortUrl.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <form class="w-full flex flex-col space-y-3 lg:space-y-0 lg:space-x-3 lg:flex-row justify-between">
+    <input placeholder="https://github.com/url-shortener-front" class="shadow-lg w-full flex-1 bg-warm-gray-50 text-warm-gray-800 placeholder-gray-500 rounded outline-none transition duration-100 px-3 py-2" />
+
+    <button class="relative px-5 py-2 font-medium text-white group flex-shrink">
+      <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-english-lavender group-hover:bg-mountbatten-pink group-hover:skew-x-12"></span>
+      <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-mountbatten-pink group-hover:bg-english-lavender group-hover:-skew-x-12"></span>
+
+      <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
+      <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-pastel-pink -rotate-12"></span>
+      <span class="relative">Short that URL</span>
+    </button>
+  </form>
+</template>
