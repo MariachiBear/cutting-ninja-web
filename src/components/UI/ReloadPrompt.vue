d11c60fc8f14e65d3e6b5c53085c1df70c943551
Rubén Conde <ruben.conde@ux-key.com>
2021-11-17 15:50:42 +0100

    fix: :bug: PWA prompt texts
    Signed-off-by: Rubén Conde <ruben.conde@ux-key.com>

--- src/components/UI/ReloadPrompt.vue
+++ src/components/UI/ReloadPrompt.vue
@@ -1,57 +1,3 @@
-<template>
-   <div
-      id="reload_prompt"
-      class="
-         4k:bottom-4 4k:right-4
-         4xl:bottom-3 4xl:right-3
-         bg-warm-gray-100
-         bottom-0
-         dark:bg-warm-gray-600
-         fixed
-         lg:bottom-2 lg:right-2 lg:w-1/4 lg:rounded
-         right-0
-         section-padding
-         w-full
-         z-1000
-         colors-300
-         shadow-2xl
-      "
-      :class="[isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none']"
-      role="alert"
-   >
-      <div class="relative py-2 px-4 text-theme">
-         <span v-if="offlineReady" class="!font-medium">
-            {{ t('label.offline_ready') }}
-         </span>
-         <span v-if="needRefresh" class="!font-medium">
-            {{ t('label.new_content') }}
-         </span>
-      </div>
-      <div
-         class="px-4 py-2 flex justify-start items-start gap-1 items-center text-theme colors-300"
-      >
-         <span v-if="offlineReady" class="!font-medium"> {{ t('label.close_in') }}: </span>
-         <span v-if="needRefresh" class="!font-medium"> {{ t('label.reload_in') }}: </span>
-
-         <span class="!font-medium">
-            <b>{{ count }}</b> {{ t('unit.second', count) }}
-         </span>
-
-         <div class="flex-grow"></div>
-
-         <button
-            v-if="!offlineReady"
-            class="hover:text-blue-500"
-            @click="updateServiceWorker(true)"
-         >
-            {{ t('button.close') }}
-         </button>
-         <button v-if="needRefresh" class="hover:text-blue-500" @click="close()">
-            {{ t('button.reload_now') }}
-         </button>
-      </div>
-   </div>
-</template>
 <script setup lang="ts">
 import { get, or, set, useCounter, useIntervalFn, whenever } from '@vueuse/core';
 import { useRegisterSW } from 'virtual:pwa-register/vue';
@@ -80,3 +26,54 @@ whenever(
    }
 );
 </script>
+
+<template>
+   <div
+      id="reload_prompt"
+      class="
+         4k:bottom-4 4k:right-4
+         4xl:bottom-3 4xl:right-3
+         all-300
+         bg-warm-gray-100
+         bottom-0
+         dark:bg-warm-gray-600
+         fixed
+         lg:bottom-2 lg:right-2 lg:w-1/4 lg:rounded lg:shadow-2xl
+         right-0
+         section-padding
+         shadow-inner
+         w-full
+         z-1000
+      "
+      :class="[isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none']"
+      role="alert"
+   >
+      <div class="relative py-2 px-4 text-theme colors-300">
+         <span v-if="offlineReady" class="!font-medium">
+            {{ t('label.offline_ready') }}
+         </span>
+         <span v-if="needRefresh" class="!font-medium">
+            {{ t('label.new_content') }}
+         </span>
+      </div>
+      <div
+         class="px-4 py-2 flex justify-start items-start gap-1 items-center text-theme colors-300"
+      >
+         <span v-if="offlineReady" class="!font-medium"> {{ t('label.close_in') }}: </span>
+         <span v-if="needRefresh" class="!font-medium"> {{ t('label.reload_in') }}: </span>
+
+         <span class="!font-medium">
+            <b>{{ count }}</b> {{ t('unit.second', count) }}
+         </span>
+
+         <div class="flex-grow"></div>
+
+         <button v-if="offlineReady" class="hover:text-blue-500" @click="updateServiceWorker(true)">
+            {{ t('button.close') }}
+         </button>
+         <button v-if="needRefresh" class="hover:text-blue-500" @click="close()">
+            {{ t('button.reload_now') }}
+         </button>
+      </div>
+   </div>
+</template>
