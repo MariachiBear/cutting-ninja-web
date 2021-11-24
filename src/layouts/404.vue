5239be99e138194a9c278e71151c9a4c8b220e42
Rubén Conde <ruben.conde@ux-key.com>
2021-11-24 11:40:39 +0100

    fix: :adhesive_bandage: Title tag
    Signed-off-by: Rubén Conde <ruben.conde@ux-key.com>

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+const { t } = useI18n();
+useHead({
+   title: `URL Shortener - ${t('label.page_not_found')}`,
+});
+</script>
+
 <template>
    <main class="h-screen max-h-screen overflow-hidden">
       <router-view />
