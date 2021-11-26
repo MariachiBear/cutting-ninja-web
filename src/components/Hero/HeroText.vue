4358b002d6f550ad84d923ce288e9166914ed0bc
Rubén Conde <dev@rubn.xyz>
2021-11-27 00:53:20 +0100

    fix: :adhesive_bandage: Slogan list update
    Signed-off-by: Rubén Conde <dev@rubn.xyz>

--- src/components/Hero/HeroText.vue
+++ src/components/Hero/HeroText.vue
@@ -1,63 +1,56 @@
 <script setup lang="ts">
 const { t } = useI18n();
 const phrases = [
-   "There's no Wrong Way to Eat a Cutting Ninja",
-   'Keep That Cutting Ninja Complexion',
-   'Cutting Ninja as sure as the sun shines',
-   'A Smooth-Running Cutting Ninja is a Relaxing Experience',
-   'Everyone should believe in Cutting Ninja',
-   'Cutting Ninja innovate your world',
-   "There's Only One Cutting Ninja",
-   'The Cutting Ninja Sign Means Happy Motoring',
-   'Get the Cutting Ninja Habit',
-   'Cutting Ninja moments',
-   'Inspired by Cutting Ninja',
-   'Everyone loves Cutting Ninja',
-   'I Scream, You Scream, We All Scream for Cutting Ninja',
-   'Prepare To Want Cutting Ninja',
-   'Reach for the Cutting Ninja',
-   'Tense, Nervous, Cutting Ninja ?',
-   'Cutting Ninja shines through',
-   'Cutting Ninja the wise choice',
-   "The Man From Cutting Ninja, he says 'Yes'",
-   'Put Cutting Ninja in your pipe and smoke it!',
-   'Cutting Ninja no one else is better',
-   "Where's Cutting Ninja ?",
-   'Cutting Ninja is so Bracing',
-   'Doing It Right Before Your Cutting Ninja',
-   'Cutting Ninja - what more could you want?',
-   "You Can't Top a Cutting Ninja",
-   'Review the facts Cutting Ninja is the best',
-   'Cutting Ninja, satisfies the need',
-   'Cutting Ninja gets me excited',
-   "Cutting Ninja, When there's nothing else",
-   'Make the Most of Cutting Ninja',
-   "Let's Cutting Ninja !",
-   'For That Deep Down Body Cutting Ninja',
-   'Cutting Ninja after a long day',
-   "Cutting Ninja - a class of it's own",
-   "Let's Face The Music and Cutting Ninja",
-   'I wish i was a Cutting Ninja',
-   'Cutting Ninja Is Our Middle Name',
-   'Be inspired by Cutting Ninja',
-   'Cutting Ninja - It Does a Body Good',
-   'Made by Cutting Ninja',
-   'Make Fun of Cutting Ninja',
-   'Grab Life by the Cutting Ninja',
-   'Give the Dog a Cutting Ninja',
-   'Cutting Ninja for hire',
-   'More Than Just a Cutting Ninja',
-   'Free for all Cutting Ninja',
-   'Cutting Ninja is what the world was waiting for',
-   'You Deserve A Cutting Ninja Today',
-   'Let your Cutting Ninja flow',
-   "It's fast, it's furious, it's Cutting Ninja",
-   "It's Good To Talk Cutting Ninja",
-   "Wouldn't You Like To Be A Cutting Ninja Too?",
-   "Life's Pretty Straight Without Cutting Ninja",
-   'Pleasing Cutting Ninja the World Over',
-   "It's my Cutting Ninja!",
-   "Life is short, just like our URL's",
+   'Get more from life with Cutting Ninja.',
+   'Cutting Ninja inside you.',
+   "It's time to think about Cutting Ninja.",
+   'Cutting Ninja, satisfies the need.',
+   "I'm Not Going to Pay A Lot For This Cutting Ninja.",
+   'How Do You Eat Your Cutting Ninja?',
+   "What's In Your Cutting Ninja?",
+   "Cutting Ninja enjoy when no-one's around.",
+   'Cutting Ninja rules.',
+   'We Bring Cutting Ninja to Life.',
+   "Mamma Mia, That'so Spicy Cutting Ninja!",
+   'Cutting Ninja, just the best.',
+   'Cutting Ninja, the fresh maker.',
+   'Go To Work On A Cutting Ninja.',
+   'I want more, I want Cutting Ninja.',
+   'Saved by Cutting Ninja',
+   'Cutting Ninja have another serving.',
+   'If You Want To Get Ahead, Get A Cutting Ninja.',
+   "Sweet as the Moment When the Cutting Ninja Went 'Pop'",
+   "I'd walk a mile for Cutting Ninja.",
+   'Simple, Impartial Cutting Ninja.',
+   'I scream, You Scream, We All Scream for Cutting Ninja.',
+   'I need Cutting Ninja right now.',
+   'Get More From Cutting Ninja.',
+   'Melts In Your Cutting Ninja, Not In Your Hand.',
+   'Cutting Ninja as sure as the sun shines.',
+   "There's Always Room For Cutting Ninja.",
+   'Cutting Ninja opens the flood gates.',
+   "Cutting Ninja, try it, you'll like it!",
+   'Where the hell is Cutting Ninja?',
+   'The one and only Cutting Ninja.',
+   'The Cutting Ninja is Mightier than the Sword.',
+   'All you require is Cutting Ninja.',
+   'Just Like Cutting Ninja Used To Make.',
+   'Cutting Ninja groove.',
+   'The top pick... Cutting Ninja.',
+   "Does the Hard Cutting Ninja, So You Don't Have To.",
+   'You Can Do It When You Cutting Ninja It.',
+   'With A Name Like Cutting Ninja, It Has To Be Good.',
+   "Mama's got the Magic of Cutting Ninja.",
+   'My Cutting Ninja.',
+   'The original Cutting Ninja.',
+   'Driven by Cutting Ninja.',
+   "Don't live a Little, live a Cutting Ninja.",
+   "I'd do anything for Cutting Ninja.",
+   'One Cutting Ninja is better than two of something else.',
+   "Cutting Ninja, it's a kind of magic.",
+   'Cutting Ninja when only the best will do.',
+   "If You've Got the Time, We've Got the Cutting Ninja.",
+   'Cutting Ninja, one for all.',
 ];
 const activePhrase = ref(phrases[Math.floor(Math.random() * phrases.length)]);
 </script>
@@ -65,17 +58,7 @@ const activePhrase = ref(phrases[Math.floor(Math.random() * phrases.length)]);
 <template>
    <div
       id="hero-text"
-      class="
-         flex flex-col
-         gap-10
-         items-center
-         justify-around
-         lg:items-start lg:text-left lg:max-w-6/12 lg:text-left
-         max-w-11/12
-         text-center
-         w-full
-         2xl:gap-15
-      "
+      class="flex flex-col gap-10 items-center justify-around lg:items-start lg:text-left lg:max-w-6/12 lg:text-left max-w-11/12 text-center w-full 2xl:gap-15"
    >
       <h1 class="2xl:text-5xl colors-300 font-bold lg:text-4xl md:text-5xl text-4xl text-theme">
          {{ activePhrase }}
