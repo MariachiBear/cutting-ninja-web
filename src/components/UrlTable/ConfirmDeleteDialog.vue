8e20266c2b69757347080a85ec9e0f6202e713c8
Rubén Conde <dev@rubn.xyz>
2021-11-29 09:40:04 +0100

    fix: :lipstick: Better delete dialog
    Signed-off-by: Rubén Conde <dev@rubn.xyz>

--- src/components/UrlTable/ConfirmDeleteDialog.vue
+++ src/components/UrlTable/ConfirmDeleteDialog.vue
@@ -4,14 +4,26 @@ const { t } = useI18n();
 
 <template>
    <div
-      class="bg-unbleached-sand dark:bg-rocket-metallic shadow-inner flex flex-row justify-center items-center gap-5 px-5"
+      class="
+         bg-unbleached-sand
+         dark:bg-rocket-metallic
+         prose
+         shadow-inner
+         flex flex-col
+         lg:flex-row
+         justify-center
+         items-center
+         lg:gap-5 lg:px-5
+      "
    >
-      <ic-outline-warning-amber class="text-8xl text-english-lavender w-1/7" />
+      <ic-outline-warning-amber
+         class="text-8xl text-red-700 dark:text-red-400 opacity-50 lg:w-1/7"
+      />
 
       <div class="text-justify w-6/7">
          <p>
             {{ t('label.delete_question') }}
-            <span class="font-medium">{{ t('label.delete_suggestion') }}</span>
+            <span class="font-medium underline">{{ t('label.delete_suggestion') }}</span>
          </p>
       </div>
    </div>
