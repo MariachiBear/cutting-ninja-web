121de34c63e083737d56dfc02ef1a633a99ae917
Rubén Conde Magaña <code@rubn.dev>
2022-07-28 02:57:23 +0200

    feat: :sparkles: Profile picture selecter
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

---
+++ src/components/Form/ProfilePictureSettings.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useUserStore } from '~/store/user';
+const userState = useUserStore.getState();
+
+const [isGravatar, toggleIsGravatar] = useToggle(userState.user?.useGravatar);
+
+watch(isGravatar, (newValue) => {
+   useUserStore.updateUser({ useGravatar: newValue });
+});
+</script>
+
+<template>
+   <div class="flex flex-col items-center justify-center space-y-5">
+      <ProfilePicture is-from-settings :is-gravatar="isGravatar" />
+
+      <div class="flex items-center justify-center w-full mb-12 text-theme text-sm">
+         <a class="mr-3 text-btn">Boring Avatars</a>
+
+         <label for="toggleB" class="flex items-center cursor-pointer">
+            <!-- toggle -->
+            <div class="relative">
+               <!-- input -->
+               <input
+                  type="checkbox"
+                  id="toggleB"
+                  class="sr-only transform"
+                  v-model="isGravatar"
+                  @input="() => toggleIsGravatar()"
+               />
+               <!-- line -->
+               <div class="block bg-rocket-metallic w-14 h-8 rounded-full"></div>
+               <!-- dot -->
+               <div
+                  class="absolute left-1 top-1 bg-pastel-pink w-6 h-6 rounded-full transition-all transform duration-150 ease-out"
+                  :class="[isGravatar ? 'translate-x-full' : '']"
+               ></div>
+            </div>
+         </label>
+         <a class="ml-3 text-btn">Gravatar</a>
+      </div>
+   </div>
+</template>
--- src/components/Navbar/UserInfo.vue
+++ src/components/Navbar/UserInfo.vue
@@ -14,11 +14,6 @@ const [isMenuOpen, toggleIsMenuOpen] = useToggle(false);
 const userInfo = ref();
 
 onClickOutside(userInfo, () => toggleIsMenuOpen(false));
-
-const sourceImg = computed(
-   () =>
-      `https://source.boringavatars.com/bauhaus/120/${userState.user?.email}?colors=E8BAA2,B5838D,4e4b53,E5989B`
-);
 </script>
 
 <template>
@@ -28,11 +23,7 @@ const sourceImg = computed(
             <span v-if="!isSmallScreen" class="max-w-100 truncate text-theme colors-300">
                {{ userState.user?.firstName }} {{ userState.user?.lastName }}
             </span>
-            <img
-               :src="sourceImg"
-               class="lg:h-7 lg:w-7 4k:w-100 4k:h-100 4xl:w-10 4xl:h-10 5xl:w-80 5xl:h-80 bg-accent bg-opacity-20 h-8 object-contain rounded-full shadow-md w-8"
-               alt="user"
-            />
+            <ProfilePicture />
          </button>
          <Dropdown :is-menu-open="isMenuOpen" />
       </div>
